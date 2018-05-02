b26d570bc8df3ea75f664d4e66e924ecccf7b4a9
dy4242407 <yue.dong2@mail.mcgill.ca>
2018-05-01 20:20:27 -0400

    add a new implementation of self attention

---
+++ Structured-Self-Attentive-Sentence-Embedding/data_loader.py
@@ -0,0 +1,81 @@
+#Please create your own dataloader for new datasets of the following type
+
+import torch, keras
+import numpy as np
+from keras.datasets import imdb
+from keras.preprocessing.sequence import pad_sequences
+import torch.utils.data as data_utils
+from .data import Data_Utility
+ 
+def load_data_set(type,max_len,vocab_size,batch_size):
+    """
+        Loads the dataset.
+        Args:
+            type: 1 for multiclass classfication return wiki set
+            max_len: {int} timesteps used for padding
+			vocab_size: {int} size of the vocabulary
+			batch_size: batch_size
+        Returns:
+            train_loader: {torch.Dataloader} train dataloader
+            x_test_pad  : padded tokenized test_data for cross validating
+			y_test      : y_test
+            word_to_id  : {dict} words mapped to indices
+ 
+      
+        """
+    INDEX_FROM=3
+    # wiki
+    # data_loc = '/home/ml/ksinha4/datasets/data_WIKI'
+    # print("----initial data loading----")
+    # dataLoader = Data_Utility()
+    # x_train_texts, y_train = dataLoader.read(data_loc=data_loc, file_name="full_docs_2_train.csv")
+    # x_val_texts, y_val = x_train_texts[:int(0.1 * len(y_train))], y_train[:int(0.1 * len(y_train))]
+    # x_train_texts, y_train = x_train_texts[int(0.1 * len(y_train)):], y_train[int(0.1 * len(y_train)):]
+    # x_test_texts, y_test = dataLoader.read(data_loc=data_loc, file_name="full_docs_2_test.csv")
+    # print("----finish data loading----{%d} train,{%d} val, {%d} test"
+    #       % (len(y_train), len(y_val), len(y_test)))
+
+    # DB pedia
+    data_loc = '/home/ml/ksinha4/mlp/hier-class/data'
+    print("----initial data loading----")
+    dataLoader = Data_Utility()
+    x_train_texts, y_train = dataLoader.read(data_loc=data_loc, file_name="df_small_train.csv")
+    x_val_texts, y_val = x_train_texts[:int(0.1*len(y_train))], y_train[:int(0.1*len(y_train))]
+    x_train_texts, y_train = x_train_texts[int(0.1 * len(y_train)):], y_train[int(0.1 * len(y_train)):]
+    x_test_texts, y_test = dataLoader.read(data_loc=data_loc, file_name="df_small_test.csv")
+    print("----finish data loading----{%d} train,{%d} val, {%d} test"
+          %(len(y_train),len(y_val),len(y_test)))
+
+
+    w2i, i2w = dataLoader.assign_word_ids(x_train_texts+x_val_texts+x_test_texts, vocab_size=vocab_size)
+    dataLoader.assign_category_ids(list(y_train) + list(y_val) + list(y_test))
+    print("----processed {%d} word_2_id, {%d}cat_2_id----"%(len(w2i),len(dataLoader.cat2id)))
+
+    x_train = dataLoader.transfer_word_to_id(x_train_texts, w2i)
+    x_val = dataLoader.transfer_word_to_id(x_val_texts, w2i)
+    x_test = dataLoader.transfer_word_to_id(x_test_texts, w2i)
+    y_train = np.array(dataLoader.transfer_cat_to_id(y_train))
+    y_val = np.array(dataLoader.transfer_cat_to_id(y_val))
+    y_test = np.array(dataLoader.transfer_cat_to_id(y_test))
+    print("----finish transfering w2i,c2i----")
+
+    x_train_pad = pad_sequences(x_train, maxlen=max_len, padding='post')
+    x_val_pad = pad_sequences(x_val, maxlen=max_len, padding='post')
+    x_test_pad = pad_sequences(x_test, maxlen=max_len, padding='post')
+
+
+    train_data = data_utils.TensorDataset(torch.from_numpy(x_train_pad).type(torch.LongTensor),
+                                          torch.from_numpy(y_train).type(torch.LongTensor))
+    val_data = data_utils.TensorDataset(torch.from_numpy(x_val_pad).type(torch.LongTensor),
+                                          torch.from_numpy(y_val).type(torch.LongTensor))
+    test_data = data_utils.TensorDataset(torch.from_numpy(x_test_pad).type(torch.LongTensor),
+                                          torch.from_numpy(y_test).type(torch.LongTensor))
+
+    train_loader = data_utils.DataLoader(train_data, batch_size=batch_size,
+                                         shuffle=True, drop_last=True)
+    val_loader = data_utils.DataLoader(val_data, batch_size=batch_size,
+                                         shuffle=True, drop_last=True)
+    test_loader = data_utils.DataLoader(test_data, batch_size=batch_size,
+                                        shuffle=True, drop_last=True)
+
+    return train_loader, val_loader, test_loader, w2i, dataLoader.cat2id
